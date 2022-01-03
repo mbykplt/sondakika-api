7f6e32b72e9bdb72e7900792ccb699e0fe5ba258
Muhammed Enes Büyükpolat <36236673+mbykplt@users.noreply.github.com>
2022-01-03 19:19:30 +0300

    Update index.js

--- index.js
+++ index.js
@@ -3,9 +3,6 @@ const cheerio=require("cheerio");
 async function getir(){
 var veri=await axios.get("https://www.sondakika.com/");
 const $=cheerio.load(veri.data);
-var numberOfData=5;
-var tarih=new Date();
-var today=tarih.getDay()+"."+tarih.getMonth+"."+tarih.getFullYear();
 for(var i=1; i<=numberOfData; i++){
 var baslik=$("#manset"+i +"> a > div.bx-caption > span").text();
 var saat=$("#manset"+i +"> a > div.Mansettime > span").text();
